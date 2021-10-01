cbe00c57e1349f02b6a67f6e150db4996a761fd3
tamsu <tamsu@LAPTOP-KH477OF4>
2021-10-01 16:39:21 +0200

    Me ha salido a la primera

---
+++ src/ejemplo03.java
@@ -0,0 +1,10 @@
+
+public class ejemplo03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Aqui se escriben las cosas");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module IFCT_Github_DQL_Ejemplo03 {
+	sysou
+}
